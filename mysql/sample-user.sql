96aa8875d48985d5ca08a44118e5c6125cc27232
Mark Roland <radiowhir@gmail.com>
2017-08-30 09:22:18 -0700

    Fixed typo in sample mysql user privileges

--- mysql/sample-user.sql
+++ mysql/sample-user.sql
@@ -2,4 +2,4 @@
 # This should be changed in a production environment
 
 GRANT USAGE ON *.* TO 'user'@'localhost' IDENTIFIED BY PASSWORD '*B4F7EAD1B3C284EDA2B9D943A7DA4C6DA61A2B44';
-GRANT ALTER ROUTINE, CREATE ROUTINE, EXECUTE, INSERT, UPDATE PRIVILEGES ON `database`.* TO 'user'@'localhost';
+GRANT ALTER ROUTINE, CREATE ROUTINE, EXECUTE, INSERT, UPDATE ON `database`.* TO 'user'@'localhost';
