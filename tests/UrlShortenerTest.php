b71c8a280fe4af98d28b3cc84067046e19cf1f8f
Mark Roland <radiowhir@gmail.com>
2017-10-17 16:49:52 -0700

    Fixed Namespace issue
     - Added database connection to test. Not tested with an actual database

--- tests/UrlShortenerTest.php
+++ tests/UrlShortenerTest.php
@@ -2,15 +2,31 @@
 
 namespace markroland\UrlShortener\Tests;
 
-class UrlShortenerTest extends PHPUnit_Framework_TestCase {
+class UrlShortenerTest extends \PHPUnit_Framework_TestCase {
 
-    protected $UrlShortener;
+    protected static $UrlShortener;
 
+    protected static $pdo_conn;
+
+    /*
     public function setup()
     {
 
-        $this->UrlShortener = new markroland\UrlShortener();
+        // Connect to database
+        try {
+            self::$pdo_conn = new \PDO(
+                'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=' . DB_CHARSET . '',
+                DB_USER,
+                DB_PASSWORD
+            );
+            self::$pdo_conn->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+        } catch (\PDOException $e) {
+            // print($e->getMessage() . PHP_EOL);
+        }
+
+        self::$UrlShortener = new \markroland\UrlShortener(self::$pdo_conn);
     }
+    //*/
 
     public function test_get_field_list()
     {
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,10 @@
 <?php
 
-// require realpath(__DIR__ . '/../src') . '/urlShortner.php';
+// require realpath(__DIR__ . '/../src') . '/urlShortener.php';
+
+// Include database credentials
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'short_url');
+define('DB_CHARSET', 'utf8');
+define('DB_USER', 'test_user');
+define('DB_PASSWORD', 'test_password');
